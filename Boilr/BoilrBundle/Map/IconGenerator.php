994683691bb045619b8b99b6ad4c5760142cc854
unixo <unixo@devzero.it>
2012-03-10 21:12:38 +0100

    generazione dell'icona in base al tipo d'indirizzo

---
+++ Boilr/BoilrBundle/Map/IconGenerator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Boilr\BoilrBundle\Map;
+
+use Boilr\BoilrBundle\Entity\Address as MyAddress;
+
+use Vich\GeographicalBundle\Map\Marker\Icon\IconGeneratorInterface;
+
+/**
+ * IconGenerator.
+ */
+class IconGenerator implements IconGeneratorInterface
+{
+    static private $type = array(
+            MyAddress::TYPE_HOME   => 'C.png',
+            MyAddress::TYPE_OFFICE => 'U.png',
+            MyAddress::TYPE_OTHER  => 'A.png'
+    );
+
+    /**
+     * {@inheritDoc}
+     */
+    public function generateIcon($obj)
+    {
+        if ($obj instanceof MyAddress) {
+            $img = self::$type[$obj->getType()];
+
+            return "/img/".$img;
+        }
+
+        return null;
+    }
+}
