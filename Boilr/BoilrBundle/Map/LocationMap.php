4e779f20ba46cf491347f1e078b623cccd5351af
unixo <unixo@devzero.it>
2012-03-10 19:48:01 +0100

    attivati controlli nella mappa per zoom e tipo visuale

--- Boilr/BoilrBundle/Map/LocationMap.php
+++ Boilr/BoilrBundle/Map/LocationMap.php
@@ -19,10 +19,14 @@ class LocationMap extends Map
         // configure your map in the constructor
         // by setting the options
 
+        $this->setHeight(400);
         $this->setAutoZoom(true);
         $this->setContainerId('addressMapContainer');
         $this->setContainerAttributes(array('class' => 'map embossin'));
         $this->setVarName("addressesMap");
+        $this->setShowZoomControl(true);
+        $this->setShowMapTypeControl(true);
+        $this->setShowInfoWindowsForMarkers(true);
 
         //$this->addMarker(new \Vich\GeographicalBundle\Map\Marker\MapMarker(41.6298297, 12.4738096));
         //$this->addMarker(new \Vich\GeographicalBundle\Map\Marker\MapMarker(41.7403131, 12.2676940));
