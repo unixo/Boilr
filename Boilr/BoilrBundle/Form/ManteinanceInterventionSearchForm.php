e55b84c794b86cfd02b195f0b8ca93a580c0a60f
unixo <unixo@devzero.it>
2012-04-21 13:21:05 +0200

    aggiunte le "label" ai campi

--- Boilr/BoilrBundle/Form/ManteinanceInterventionSearchForm.php
+++ Boilr/BoilrBundle/Form/ManteinanceInterventionSearchForm.php
@@ -4,39 +4,42 @@ namespace Boilr\BoilrBundle\Form;
 
 use Symfony\Component\Form\AbstractType,
     Symfony\Component\Form\FormBuilder;
-
 use Boilr\BoilrBundle\Entity\ManteinanceIntervention;
 
 class ManteinanceInterventionSearchForm extends AbstractType
 {
+
     public function buildForm(FormBuilder $builder, array $options)
     {
         $allStatus = array(
-                    ManteinanceIntervention::STATUS_ABORTED   => 'Annullata',
-                    ManteinanceIntervention::STATUS_CLOSED    => 'Conclusa',
-                    ManteinanceIntervention::STATUS_CONFIRMED => 'Confermato',
-                    ManteinanceIntervention::STATUS_SUSPENDED => 'Sospeso',
-                    ManteinanceIntervention::STATUS_TENTATIVE => 'Da confermare'
-                          );
+            ManteinanceIntervention::STATUS_ABORTED => 'Annullata',
+            ManteinanceIntervention::STATUS_CLOSED => 'Conclusa',
+            ManteinanceIntervention::STATUS_CONFIRMED => 'Confermato',
+            ManteinanceIntervention::STATUS_SUSPENDED => 'Sospeso',
+            ManteinanceIntervention::STATUS_TENTATIVE => 'Da confermare'
+        );
 
         $builder->add('searchByDate', 'checkbox', array('required' => false))
-                ->add('startDate', 'datetime', array(
-                                                'required'    => false,
-                                                'date_format' => 'dd/MM/yyyy',
-                                                'date_widget' => 'single_text'
-                    ))
-                ->add('endDate', 'datetime', array(
-                                                'required' => false,
-                                                'date_format' => 'dd/MM/yyyy',
-                                                'date_widget' => 'single_text'
-                    ))
-                ->add('planned', 'checkbox', array('required' => false))
-                ->add('status', 'choice', array(
-                                    'choices'   => $allStatus,
-                                    'multiple'  => true,
-                                    'required'  => false,
+                ->add('startDate', 'date', array(
+                    'label' => 'Inizio intervallo',
+                    'required' => false,
+                    'format' => 'dd/MM/yyyy',
+                    'widget' => 'single_text'
                 ))
-                ;
+                ->add('endDate', 'date', array(
+                    'label' => 'Fine intervallo',
+                    'required' => false,
+                    'format' => 'dd/MM/yyyy',
+                    'widget' => 'single_text',
+                ))
+                ->add('planned', 'checkbox', array('required' => false, 'label' => 'Programmato'))
+                ->add('status', 'choice', array(
+                    'label' => 'Stato',
+                    'choices' => $allStatus,
+                    'multiple' => true,
+                    'required' => false,
+                ))
+        ;
     }
 
     public function getName()
@@ -44,9 +47,11 @@ class ManteinanceInterventionSearchForm extends AbstractType
         return 'interventionSearchForm';
     }
 
-    public function getDefaultOptions(array $options) {
+    public function getDefaultOptions(array $options)
+    {
         return array(
             'data_class' => 'Boilr\BoilrBundle\Form\Model\ManteinanceInterventionFilter'
         );
     }
+
 }
