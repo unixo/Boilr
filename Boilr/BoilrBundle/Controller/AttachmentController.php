939ba11f9a11bad29782b223ec6b460cf277318b
unixo <unixo@devzero.it>
2012-04-24 19:08:45 +0200

    - nuova entità "Attachment" - creazione relativo controller e view

---
+++ Boilr/BoilrBundle/Controller/AttachmentController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Boilr\BoilrBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+    Symfony\Component\Security\Core\SecurityContext,
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\Template,
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter,
+    JMS\SecurityExtraBundle\Annotation\Secure,
+    Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Boilr\BoilrBundle\Entity\Attachment as MyAttachment,
+    Boilr\BoilrBundle\Entity\System,
+    Boilr\BoilrBundle\Entity\Group,
+    Boilr\BoilrBundle\Entity\ManteinanceIntervention,
+    Boilr\BoilrBundle\Form\AttachmentForm;
+
+/**
+ * \Boilr\BoilrBundle\Controller\AttachmentController
+ *
+ * @author unixo
+ */
+class AttachmentController extends BaseController
+{
+    function __construct()
+    {
+        $this->entityName = 'BoilrBundle:Attachment';
+    }
+
+    /**
+     * @Route("/{id}/delete", name="attachment_delete")
+     * @ParamConverter("attachment", class="BoilrBundle:Attachment")
+     * @Secure(roles="ROLE_ADMIN, ROLE_SUPERUSER, ROLE_INSTALLER")
+     * @Template()
+     */
+    public function deleteAction(MyAttachment $attachment)
+    {
+        // Check file ownership if current user is an installer
+        $currentUser = $this->getCurrentUser();
+        if ($currentUser->hasRole(Group::ROLE_INSTALLER)) {
+            if ($this->getCurrentUser()->getLogin() !=  $attachment->getOwner()->getLogin()) {
+                throw new AccessDeniedException('permission denied: not owner');
+            }
+        }
+
+        try {
+            $dem = $this->getEntityManager();
+            $dem->remove($attachment);
+            $dem->flush();
+            $this->setNoticeMessage("Allegato eliminato con successo");
+        } catch (Exception $exc) {
+            $this->setErrorMessage("Si è verificato un errore durante l'eliminazione.");
+        }
+
+        return $this->getLastRoute();
+    }
+
+
+    /**
+     * @Route("/{id}/upload", name="system_upload_doc")
+     * @ParamConverter("system", class="BoilrBundle:System")
+     * @Template()
+     */
+    public function uploadSystemDocAction(System $system)
+    {
+        // create and init attachment
+        $attachment = new MyAttachment();
+        $attachment->setUploadDate(new \DateTime());
+        $attachment->setType(MyAttachment::TYPE_SYSTEM);
+        $attachment->setOwner($this->getCurrentUser());
+        $attachment->setParentSystem($system);
+
+        // build the form and pass data to it
+        $form = $this->createForm(new AttachmentForm(), $attachment);
+
+        if ($this->isPOSTRequest()) {
+            $form->bindRequest($this->getRequest());
+
+            if ($form->isValid()) {
+                try {
+                    $dem = $this->getDoctrine()->getEntityManager();
+                    $dem->persist($attachment);
+                    $dem->flush();
+                    $this->setNoticeMessage('Documento allegato con successo');
+
+                    return $this->redirect($this->generateUrl('system_list_doc', array('id' => $system->getId())));
+                } catch (Exception $exc) {
+                    $this->setErrorMessage('Si è verificato un errore durante il salvataggio');
+                }
+            }
+        }
+
+        return array('form' => $form->createView(), 'system' => $system);
+    }
+
+    /**
+     * @Route("/{id}/upload", name="intervention_upload_doc")
+     * @ParamConverter("intervention", class="BoilrBundle:ManteinanceIntervention")
+     * @Template()
+     */
+    public function uploadInterventionDocAction(ManteinanceIntervention $intervention)
+    {
+        // create and init attachment
+        $attachment = new MyAttachment();
+        $attachment->setUploadDate(new \DateTime());
+        $attachment->setType(MyAttachment::TYPE_INTERVENTION);
+        $attachment->setOwner($this->getCurrentUser());
+        $attachment->setParentIntervention($intervention);
+
+        // build the form and pass data to it
+        $form = $this->createForm(new AttachmentForm(), $attachment);
+
+        if ($this->isPOSTRequest()) {
+            $form->bindRequest($this->getRequest());
+
+            if ($form->isValid()) {
+                try {
+                    $dem = $this->getDoctrine()->getEntityManager();
+                    $dem->persist($attachment);
+                    $dem->flush();
+                    $this->setNoticeMessage('Documento allegato con successo');
+
+                    return $this->redirect($this->generateUrl('intervention_list_doc', array('id' => $intervention->getId())));
+                } catch (Exception $exc) {
+                    $this->setErrorMessage('Si è verificato un errore durante il salvataggio');
+                }
+            }
+        }
+
+        return array('form' => $form->createView(), 'system' => $system);
+    }
+}
---
+++ Boilr/BoilrBundle/Entity/Attachment.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace Boilr\BoilrBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM,
+    Symfony\Component\Validator\Constraints as Assert,
+    Symfony\Component\Validator\ExecutionContext,
+    Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Boilr\BoilrBundle\Entity\Attachment
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="attachments")
+ * @Vich\Uploadable
+ */
+class Attachment
+{
+
+    const TYPE_SYSTEM = 1;
+    const TYPE_INTERVENTION = 2;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     * @Assert\NotBlank
+     */
+    protected $name;
+
+    /**
+     * @Assert\File(
+     *     maxSize="3M",
+     *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg", "application/pdf", "text/plain", "application/msword"}
+     * )
+     * @Vich\UploadableField(mapping="attachment_file", fileNameProperty="name")
+     *
+     * @var File $document
+     */
+    protected $document;
+
+    /**
+     * @var type
+     *
+     * @ORM\Column(name="type", type="integer", nullable=false)
+     * @Assert\Choice(choices = {"1", "2"})
+     */
+    protected $type;
+
+    /**
+     * @var onwer
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=true)
+     */
+    protected $owner;
+
+    /**
+     * @var datetime $uploadDate
+     *
+     * @ORM\Column(name="upload_date", type="datetime", nullable=false)
+     * @Assert\NotBlank
+     */
+    protected $uploadDate;
+
+    /**
+     * @var parentSystem
+     *
+     * @ORM\ManyToOne(targetEntity="System")
+     * @ORM\JoinColumn(name="system_id", referencedColumnName="id", nullable=true)
+     */
+    protected $parentSystem;
+
+    /**
+     * @var parentIntervention
+     *
+     * @ORM\ManyToOne(targetEntity="ManteinanceIntervention")
+     * @ORM\JoinColumn(name="intervention_id", referencedColumnName="id", nullable=true)
+     */
+    protected $parentIntervention;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get type
+     *
+     * @return integer
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set uploadDate
+     *
+     * @param datetime $uploadDate
+     */
+    public function setUploadDate($uploadDate)
+    {
+        $this->uploadDate = $uploadDate;
+    }
+
+    /**
+     * Get uploadDate
+     *
+     * @return datetime
+     */
+    public function getUploadDate()
+    {
+        return $this->uploadDate;
+    }
+
+    /**
+     * Set owner
+     *
+     * @param Boilr\BoilrBundle\Entity\User $owner
+     */
+    public function setOwner(\Boilr\BoilrBundle\Entity\User $owner)
+    {
+        $this->owner = $owner;
+    }
+
+    /**
+     * Get owner
+     *
+     * @return Boilr\BoilrBundle\Entity\User
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * Set parentSystem
+     *
+     * @param Boilr\BoilrBundle\Entity\System $parentSystem
+     */
+    public function setParentSystem(\Boilr\BoilrBundle\Entity\System $parentSystem)
+    {
+        $this->parentSystem = $parentSystem;
+    }
+
+    /**
+     * Get parentSystem
+     *
+     * @return Boilr\BoilrBundle\Entity\System
+     */
+    public function getParentSystem()
+    {
+        return $this->parentSystem;
+    }
+
+    public function getDocument()
+    {
+        return $this->document;
+    }
+
+    public function setDocument($document)
+    {
+        $this->document = $document;
+    }
+
+
+    /**
+     * Set parentIntervention
+     *
+     * @param Boilr\BoilrBundle\Entity\ManteinanceIntervention $parentIntervention
+     */
+    public function setParentIntervention(\Boilr\BoilrBundle\Entity\ManteinanceIntervention $parentIntervention)
+    {
+        $this->parentIntervention = $parentIntervention;
+    }
+
+    /**
+     * Get parentIntervention
+     *
+     * @return Boilr\BoilrBundle\Entity\ManteinanceIntervention
+     */
+    public function getParentIntervention()
+    {
+        return $this->parentIntervention;
+    }
+}
