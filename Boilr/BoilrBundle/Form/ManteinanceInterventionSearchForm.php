eec0c12aa45d32608cc12c729877a3187dfbf450
unixo <unixo@devzero.it>
2012-05-02 15:25:18 +0200

    checkbox multiple per lo stato

--- Boilr/BoilrBundle/Form/ManteinanceInterventionSearchForm.php
+++ Boilr/BoilrBundle/Form/ManteinanceInterventionSearchForm.php
@@ -38,6 +38,7 @@ class ManteinanceInterventionSearchForm extends AbstractType
                     'label' => 'Stato',
                     'choices' => $allStatus,
                     'multiple' => true,
+                    'expanded' => true,
                     'required' => false,
                 ))
         ;
